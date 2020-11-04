a5492771f196ead49330941aca22b3a7090eda07
Lorenzo Jácome <lorenzo.jacome@hotmail.com>
2020-11-04 15:29:54 -0600

    Add Persitence Module files
    We uploaded the Persistence Module files. This includes a yaml example and a main exmaple.

---
+++ Data_Persistency/OpenCVExample/OpenCVExample/main.cpp
@@ -0,0 +1,22 @@
+#include <opencv2/opencv.hpp>
+#include "PersistenceModule.hpp"
+
+using namespace cv;
+using namespace std;
+
+
+//PRESENTATION EXAMPLE:
+int main() {
+	//Opens your webcam and stores what it sees in a Mat
+	VideoCapture cap(0);
+	Mat frame;
+	cap >> frame;
+
+	//Creates a persistence object, and registers a new client in memory, or delete a client using its key as parameter, and then you write to disk.
+	Persistence newClient;
+	//newClient.registerClient("A0102777", "Leo", "IMI", "a0102777@itesm.mx", true, frame);
+	newClient.deleteClient("A5");
+	newClient.writeToDisc();
+	
+	return 0;
+}
