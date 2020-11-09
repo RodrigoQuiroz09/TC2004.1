ff6971b73479fa5080d9631260271b17a040145f
AbrahamGarcia22 <A01023256@itesm.mx>
2020-11-09 14:16:03 -0600

    Pruebas con flann

--- Integration/main.cpp
+++ Integration/main.cpp
@@ -1,7 +1,13 @@
 #include "../Data_Persistency/OpenCVExample/OpenCVExample/PersistenceModule.hpp"
 #include "../Feature_Extraction/source/moduleFE.hpp"
+
+
+#include <opencv2/flann.hpp>
+
+
 using namespace cv;
 using namespace std;
+
 int main(int argc, char **argv)
 {
     if (argc != 3)
@@ -16,18 +22,26 @@ int main(int argc, char **argv)
     Mat frame2 = imread(argv[2]);
     Mat vector1 = F.getFeatures(frame);
     Mat vector2 = F.getFeatures(frame2);
-    int result = F.comparison(vector1, vector2);
+   // int result = F.comparison(vector1, vector2);
     Persistence newClient;
-    newClient.registerClient("A0000000", "KAT", "RSRSRS", "a010277fff7@itesm.mx", true, vector1);
+    //newClient.registerClient("A1111111", "MAIKOL", "ITI", "a010516f7@itesm.mx", true, vector1);
 	//newClient.deleteClient("A5");
-	newClient.writeToDisc();
+	//newClient.writeToDisc();
+    Mat fin=vector1;
+    hconcat(vector1, vector2, fin);
+    //fin.push_back(vector2);
 
-    // cout << vector1.size << endl;
+     cout << fin.size << endl;
     // cout << vector2.size << endl;
 
     // printf("The result of the module is: %d\n", result);
     // cout << "M = " << endl
     //      << " " << vector1 << endl;
+    cv::flann::GenericIndex<cvflann::L2<float>> index(fin,cvflann::KDTreeIndexParams());
+    int max_neigh=1;
+    
 
-    return 0;
+
+    
+
 }
