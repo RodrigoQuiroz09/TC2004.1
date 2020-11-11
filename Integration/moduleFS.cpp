0600d32658c90af86877a42ef805e319769b0040
Isaac Garza <isaacg99@gmail.com>
2020-11-10 21:42:03 -0600

    Chance jala

--- Integration/moduleFS.cpp
+++ Integration/moduleFS.cpp
@@ -2,20 +2,31 @@
 #include <opencv2/opencv.hpp>
 #include "PersistenceModule.hpp"
 #include <opencv2/flann.hpp>
-cv::flann::GenericIndex<cvflann::L2<float>> inde(cv::Mat allFeatures,cvflann::KDTreeIndexParams());
+#include <iostream>
 
-void FastSearch::loadUsers(std::map<std::string, Subject> users){
+void FastSearch::loadUsers(std::map<std::string, Subject> users)
+{
     std::map<std::string, Subject>::iterator itr;
-    for (itr = users.begin(); itr != users.end(); ++itr){
-        cv::hconcat(allFeatures,itr->second.face,allFeatures);
+    for (itr = users.begin(); itr != users.end(); ++itr)
+    {
+        cv::hconcat(allFeatures, itr->second.face, allFeatures);
     }
-    allFeatures=allFeatures.t();
+    allFeatures = allFeatures.t();
 }
-// void FastSearch::getIndex(cv::Mat matToIndex){
+void FastSearch::getIndex(cv::Mat matToIndex)
+{
+    index(matToIndex, cvflann::KDTreeIndexParams());
+}
+cv::Mat FastSearch::search(cv::Mat query)
+{
+    index.knnSearch(query.t(), indices, distances, max_neigh, cvflann::SearchParams());
+}
 
-// }
-// cv::Mat FastSearch::search(cv::Mat query){
-//     inde.k
-
-    
-// }
+void FastSearch::getMatches()
+{
+    for (int i = 0; i < indices.size(); i++)
+    {
+        key = "A" + std::to_string(indices.at<int>(i);
+        cout << newClient.users.find(key)->second.name << " " << newClient.users.find(key)->second.id << endl;
+    }
+}
--- Integration/moduleFS.hpp
+++ Integration/moduleFS.hpp
@@ -1,22 +1,33 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/flann.hpp>
-    class FastSearch{
-        public:
-            FastSearch(){
-                max_neighbors=10;
-                cv::Mat indices(1,max_neighbors,CV_32S);
-                cv::Mat_<float> distances(1,max_neighbors,CV_32S);
-            }
-            FastSearch(int max_neighbors){
-                this->max_neighbors=max_neighbors;
-                cv::Mat indices(1,max_neighbors,CV_32S);
-                cv::Mat_<float> distances(1,max_neighbors,CV_32S);
-            }
-            int max_neighbors;
-            void loadUsers(std::map<std::string, Subject> users);
-            void getIndex(cv::Mat matToIndex);
-            cv::Mat search(cv::Mat query);
-            cv::Mat allFeatures;
-            // cv::flann::GenericIndex<cvflann::L2<float>> index(cv::Mat allFeatures,cvflann::KDTreeIndexParams());
-            
-    };
+#include "../Data_Persistency/OpenCVExample/OpenCVExample/PersistenceModule.hpp"
+#include <string>
+class FastSearch
+{
+private:
+    cv::flann::GenericIndex<cvflann::L2<float>> index(cv::Mat &matToIndex, cvflann::KDTreeIndexParams());
+
+public:
+    FastSearch()
+    {
+        Persistence newClient;
+        max_neighbors = 10;
+        cv::Mat indices(1, max_neighbors, CV_32S);
+        cv::Mat_<float> distances(1, max_neighbors, CV_32S);
+    }
+
+    FastSearch(int max_neighbors)
+    {
+        Persistence newClient;
+        this->max_neighbors = max_neighbors;
+        cv::Mat indices(1, max_neighbors, CV_32S);
+        cv::Mat_<float> distances(1, max_neighbors, CV_32S);
+    }
+    int max_neighbors;
+    std::string key;
+    void loadUsers(std::map<std::string, Subject> users);
+    void getIndex(cv::Mat matToIndex);
+    void getMatches();
+    cv::Mat search(cv::Mat query);
+    cv::Mat allFeatures;
+};
