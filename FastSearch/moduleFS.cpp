a398186753de34eb03364a09ad5132355a611c49
AbrahamGarcia22 <A01023256@itesm.mx>
2020-11-25 18:00:11 -0600

    comments

--- FastSearch/moduleFS.cpp
+++ FastSearch/moduleFS.cpp
@@ -13,10 +13,10 @@ FastSearch::FastSearch(int neighbors)
     distances = new cv::Mat_<float>(1, max_neighbors, CV_32S);
 }
 
+
 cv::Mat FastSearch::searchIndex(cv::flann::GenericIndex<cvflann::L2<float>>* matIndex, cv::Mat query)
 {
     matIndex->knnSearch(query.t(), *indices, *distances, max_neighbors, cvflann::SearchParams());
-    // puts("Aqui todo bien");
     return *indices;
 }
 
--- FastSearch/moduleFS.hpp
+++ FastSearch/moduleFS.hpp
@@ -11,8 +11,23 @@ private:
     int max_neighbors;
 
 public:
+    /*FastSearch: constructor, it inizializes the indices and set a default int max_neighbors
+    *@param none
+    *@return nothing
+    */
     FastSearch();
+
+    /*FastSearch: constructor, it initializes the indices and set a int max_neighbors
+    *@param int neighbors 
+    *@return nothing
+    */
     FastSearch(int max_neighbors);
+
+    /* Mat searchIndex: Creates an index out of the data base searching for the mats that are similar to the query
+    *@param matIndex, flann index of a mat
+    *@param Mat query, mat to search in the index above
+    *@return mat *indices (close)
+    */
     cv::Mat searchIndex(cv::flann::GenericIndex<cvflann::L2<float>>* matIndex, cv::Mat query);
 };
 
